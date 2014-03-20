f32f984e0fa26a6ce7b3564c1a8f0a0fe5a9381b
Taras Syvash <tcomlab@gmail.com>
2014-03-20 12:36:19 +0200

    Init2

---
+++ SystemInformer/SystemInformer/Informer.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemInformer
+{
+    /// <summary>
+    /// Interaction logic for Informer.xaml
+    /// </summary>
+    public partial class Informer : Window
+    {
+        Timer tm = new Timer();
+
+        public Informer(int timeShowSec)
+        {
+            InitializeComponent();
+            var W = SystemParameters.PrimaryScreenWidth;
+            var H = SystemParameters.PrimaryScreenHeight;
+            Left = W - 350;
+            Top = H - 200;
+            tm.Elapsed += TimeTick;
+            tm.Interval = 1000*timeShowSec;
+            tm.Start();
+        }
+
+        private void TimeTick(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(Close);
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+    }
+}
